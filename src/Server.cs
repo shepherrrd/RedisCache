607ed7969b7157fc6dd85b8b03571ed0a49952f8
shepherrrd <shefkid2@gmail.com>
2024-05-19 11:09:31 +0100

    finished

--- src/Server.cs
+++ src/Server.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
@@ -7,16 +8,8 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 var arg = Console.ReadLine()!.Split(' ');
-if ( arg.Length == 1 && arg[0] == "PING" )
-{
-    if(server.Server.IsBound && arg[1] == null)
-    {
-        Console.WriteLine("+PONG\r\n");
-    }
-    else
-    {
-        Console.WriteLine(arg[1] + "\r\n");
-    }
-}
+
 server.Start();
 server.AcceptSocket(); // wait for client
+var client = server.AcceptSocket();
+await client.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n"), SocketFlags.None);
