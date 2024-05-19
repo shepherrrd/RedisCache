02787b33e9df8ac4be081ecfde4189a2018805a3
shepherrrd <shefkid2@gmail.com>
2024-05-19 11:15:21 +0100

    fiished

--- src/Server.cs
+++ src/Server.cs
@@ -7,9 +7,8 @@ Console.WriteLine("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
-var arg = Console.ReadLine()!.Split(' ');
 
-server.Start();
-server.AcceptSocket(); // wait for client
-var client = server.AcceptSocket();
-await client.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n"), SocketFlags.None);
+server.Start(); // wait for client
+var socket = server.AcceptSocket(); // wait for client
+socket.Send(Encoding.UTF8.GetBytes("+PONG\r\n"));
+socket.Close();
