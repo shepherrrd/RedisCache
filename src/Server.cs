51ea488cf133450cc35ef49d66d726916a252294
shepherrrd <shefkid2@gmail.com>
2024-05-19 11:06:15 +0100

    finished

--- src/Server.cs
+++ src/Server.cs
@@ -6,5 +6,17 @@ Console.WriteLine("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
+var arg = Console.ReadLine()!.Split(' ');
+if ( arg.Length == 1 && arg[0] == "PING" )
+{
+    if(server.Server.IsBound && arg[1] == null)
+    {
+        Console.WriteLine("+PONG\r\n");
+    }
+    else
+    {
+        Console.WriteLine(arg[1] + "\r\n");
+    }
+}
 server.Start();
 server.AcceptSocket(); // wait for client
